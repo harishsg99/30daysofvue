311a91512afa3246e1009c4a9187fc980cf1fc30
harishsg99 <40058558+harishsg99@users.noreply.github.com>
2021-05-14 11:28:52 +0530

    Hello world I am learning vue.js

---
+++ day-01/README.md
@@ -0,0 +1,3 @@
+<h3>Day 01 - What is Vue?</h3>
+
+Vue.js is a great frontend library 
---
+++ day-01/src/main.js
@@ -0,0 +1,8 @@
+new Vue({
+  el: '#app',
+  data: {
+    greetings: 'Hello World! I accepted challenge to learn vue.js',
+  },
+});
+
+
